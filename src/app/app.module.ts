a9467d03aa3ebac73d5e82752e2dc8ae003585d9
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2018-03-12 16:50:33 +0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { SelectPageComponent } from './select-page/select-page.component';
+import { BasketPageComponent } from './basket-page/basket-page.component';
 
+import {MaterialModule} from './material.module';
+import {BROWSER_ANIMATIONS_PROVIDERS} from '@angular/platform-browser/animations/src/providers';
+import {DataService} from './data.service';
+import {OrderService} from './order.service';
+import {CommonModule} from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectPageComponent,
+    BasketPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+	  AppRoutingModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService, OrderService],
+  // providers: [BROWSER_ANIMATIONS_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
